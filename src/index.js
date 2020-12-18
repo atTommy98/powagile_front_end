2cc06ca6aebf041e9a48d1601c08dcc800fd87f9
kawalpreet <xx-sona-xx@live.co.uk>
2020-12-18 16:25:39 +0000

    loging part 1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-6mmowvjn.eu.auth0.com"
+      clientId="BXfGlx4LWMwxeBvfNJl5mDya5yoDCOEp"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
