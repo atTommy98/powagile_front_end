363b1b3b47b79bf269463e98582cc3b752e4a835
Tommy Holt <tommy_holt@icloud.com>
2020-12-21 15:54:21 +0000

    Login context (User Info component obsolete?)

--- src/components/Login/UserInfo/UserInfo.js
+++ src/components/Login/UserInfo/UserInfo.js
@@ -11,6 +11,7 @@ export default function UserInfo() {
     return <div>Loading ...</div>;
   }
 
+    
   return (
     isAuthenticated && (
       <div id="userInfo">
--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,16 +1,22 @@
 // React
-import React from "react";
+import React, { useContext } from "react";
 
 // CSS
 import "./UserPage.css";
 
-import UserInfo from "../../components/Login/UserInfo/UserInfo"
+// Context
+import { UserContext } from "../../UserContext";
 
 export default function UserPage() {
+  const user = useContext(UserContext);
+
+  console.log(user);
+
   return (
     <div>
       <h1>User Information</h1>
-      <UserInfo></UserInfo>
+      <p>{user[0] === undefined ? `Please Log In` : user[0].email}</p>
+      <p>{`User logged in: ${user[1]}`}</p>
     </div>
   );
 }
