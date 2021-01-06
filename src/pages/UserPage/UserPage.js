434c412ea79d127af896ba28669f2afec54af2e0
Tommy Holt <tommy_holt@icloud.com>
2021-01-06 16:14:22 +0000

    Chart stuffs on User Page

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,5 +1,5 @@
 // React
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // CSS
 import "./UserPage.css";
@@ -7,8 +7,37 @@ import "./UserPage.css";
 // Auth0
 import { useAuth0 } from "@auth0/auth0-react";
 
+// React-Charts
+import { Line, Bar } from "react-chartjs-2";
+
 export default function UserPage() {
   const { user, isAuthenticated } = useAuth0();
+  const [stats, setStats] = useState("");
+
+  useEffect(() => {
+    fetch("https://powagile-back-end.herokuapp.com/meeting")
+      .then((res) => res.json())
+      .then((data) => setStats(data));
+  }, []);
+
+  console.log(stats);
+  console.log(stats[4].meetingStartTime)
+
+  const data = {
+    labels: ["Stand Ups", "Retros"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderWidth: 1,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        minBarLength: 0,
+        data: [2, 3],
+      },
+    ],
+  };
 
   return isAuthenticated ? (
     <div>
@@ -16,6 +45,16 @@ export default function UserPage() {
         <img src={user.picture} alt={user.name} id="userImg" />
         <h4>{user.name}</h4>
         <p>{user.email}</p>
+      </div>
+      <div>
+        <Bar
+          data={data}
+          options={{
+            scales: {
+              yAxes: [{ ticks: { suggestedMin: 0, suggestedMax: 5 } }],
+            },
+          }}
+        />
       </div>
     </div>
   ) : (
