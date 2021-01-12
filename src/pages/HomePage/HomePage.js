9a9ce3a2a8e47930c5728c3d8ad564047035c5eb
Stefan Kudev <stefan.yanchev.kudev@gmail.com>
2021-01-12 12:06:56 +0000

    homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -15,6 +15,9 @@ import Typography from "@material-ui/core/Typography";
 import ProductTitle from "../../components/ProductTitle/ProductTitle";
 import PlatformLogo from "./images/logo512.png";
 
+// React Router
+import { Link } from "react-router-dom";
+
 // CSS
 import "./HomePage.css";
 
@@ -63,43 +66,47 @@ export default function Index(props) {
       <section className="productButtonsSection">
         <Grid container spacing={3}>
           <Grid item xs>
-            <Card elevation={3}>
-              <CardActionArea>
-                <CardContent className="homeStandUpCard">
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                    style={{ fontSize: "1.2em" }}
-                  >
-                    <ProductTitle title="StandUp" />
-                  </Typography>
-                  <Typography variant="h5" component="p">
-                    Our formula for fast and engaging remote standups.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <Link className="linkWrapper" to="/rituals/standup">
+              <Card elevation={3}>
+                <CardActionArea>
+                  <CardContent className="homeStandUpCard">
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                      style={{ fontSize: "1.2em" }}
+                    >
+                      <ProductTitle title="StandUp" />
+                    </Typography>
+                    <Typography variant="h5" component="p">
+                      Our formula for fast and engaging remote standups.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           </Grid>
           <Grid item xs>
-            <Card elevation={3}>
-              <CardActionArea>
-                <CardContent className="homeRetroCard">
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                    style={{ fontSize: "1.2em" }}
-                  >
-                    <ProductTitle title="Retrospective" />
-                  </Typography>
-                  <Typography variant="h5" component="p">
-                    Real time, collaborative, and engaging retros that make a
-                    positive impact on your team.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <Link className="linkWrapper" to="/rituals/retro">
+              <Card elevation={3}>
+                <CardActionArea>
+                  <CardContent className="homeRetroCard">
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                      style={{ fontSize: "1.2em" }}
+                    >
+                      <ProductTitle title="Retrospective" />
+                    </Typography>
+                    <Typography variant="h5" component="p">
+                      Real time, collaborative, and engaging retros that make a
+                      positive impact on your team.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           </Grid>
         </Grid>
       </section>
@@ -115,58 +122,3 @@ export default function Index(props) {
     </div>
   );
 }
-
-// export default function Index(props) {
-//   return (
-//     <React.Fragment>
-//       <Features4
-//         content={{
-//           header: "There's no I in PowerShell Rangers",
-//           "col1-header": "Stay Organised",
-//           "col1-desc":
-//             "Rough night of drinking last night? Don't want to plan your stand-ups and retros? No problem. Just log in, open up your agile ritual of choice and you're good to go!",
-//           "col2-header": "Improve Your Team",
-//           "col2-desc":
-//             "Engagement data ready to view instantly. Find out who's been watching TikToks instead of attending your morning stand-ups.",
-//           "col3-header": "Make It Fun",
-//           "col3-desc":
-//             "With gamification at the heart of our work, we hope your team will have more fun using our app, keeping engagement levels high!",
-//           "col4-header": "Remote Access",
-//           "col4-desc":
-//             "Designed around a remote world, never fear that your team members are unable to contribute. It will feel like your async team members are right there with you.",
-//         }}
-//       />
-
-//       <Team2
-//         content={{
-//           "01_image":
-//             "https://ca.slack-edge.com/T6L933W4X-U01ABL9U2QZ-cf9a17f073c1-512",
-//           "01_name": "Tommy Holt",
-//           "01_job": "Dev",
-//           "02_image":
-//             "https://ca.slack-edge.com/T6L933W4X-U01A16393UK-e3bc61203f3b-512",
-//           "02_name": "Daniela Leva",
-//           "02_job": "Also a Dev",
-//           "03_image":
-//             "https://ca.slack-edge.com/T6L933W4X-U019V870Z4M-gf4edd840c04-512",
-//           "03_name": "Stefan Kudev",
-//           "03_job": "Dev #3",
-//           "04_image":
-//             "https://ca.slack-edge.com/T6L933W4X-U01A5FT1UDA-77bc2aa61ade-512",
-//           "04_name": "Kawalpreet Kaur",
-//           "04_job": "DEV",
-//           "05_image": "/images/jon.png",
-//           "05_name": "Jon Owens",
-//           "05_job": "Devdevdev",
-//         }}
-//       />
-
-//       <Gallery2 content={null} />
-
-//       <Footer3 content={null} />
-
-//       <UserDashboard></UserDashboard>
-
-//     </React.Fragment>
-//   );
-// }
